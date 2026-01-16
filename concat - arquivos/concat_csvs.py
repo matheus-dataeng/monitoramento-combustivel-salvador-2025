b5e6ed7e09f81fe8cc4682ffc3799b45eee73931
Matheus Meneses <matheusmeneses134@gmail.com>
2026-01-16 10:52:41 -0300

    Concatenação dos arquivos dos 2 semestres de 2025

---
+++ concat - arquivos/concat_csvs.py
@@ -0,0 +1,18 @@
+#CONCATENAÇÃO DOS ARQUIVOS QUE REPRESENTAM OS 2 SEMESTRES DE 2025
+import pandas as pd 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+caminho_semestre1 = os.getenv("CSV_PATH_01")
+caminho_semestre2 = os.getenv("CSV_PATH_02")
+
+df_semestre1 = pd.read_csv(caminho_semestre1, delimiter=';', encoding="latin1")
+df_semestre2 = pd.read_csv(caminho_semestre2, delimiter=';', encoding="latin1")
+
+df = pd.concat([df_semestre1, df_semestre2], ignore_index=True)
+caminho_pasta = os.getenv("DIR_PATH_CSV")
+nome_arquivo = "Preços anuais - AUTOMOTIVOS_2025.csv"
+caminho_completo = os.path.join(caminho_pasta, nome_arquivo)
+df.to_csv(caminho_completo, sep=";", encoding="latin1", index= False)
+print("Arquivo gerado")
